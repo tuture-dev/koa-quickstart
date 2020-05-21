f1e6778852a7d4fb38feba304d0e5c4d20dcc927
mRcfps <1402491442@qq.com>
2020-05-21 16:47:26 +0800

    起步

---
+++ src/server.ts
@@ -0,0 +1,18 @@
+import Koa from 'koa';
+import cors from '@koa/cors';
+import bodyParser from 'koa-bodyparser';
+
+// 初始化 Koa 应用实例
+const app = new Koa();
+
+// 注册中间件
+app.use(cors());
+app.use(bodyParser());
+
+// 响应用户请求
+app.use((ctx) => {
+  ctx.body = 'Hello Koa';
+});
+
+// 运行服务器
+app.listen(3000);
